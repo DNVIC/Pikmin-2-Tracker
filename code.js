6f3c234536081a8793fb72406e36767a41c15b8d
DNVIC <37513362+DNVIC@users.noreply.github.com>
2019-04-16 12:23:30 -0400

    Create code.js
    when I get home going to change it so all 200+ things are here

---
+++ code.js
@@ -0,0 +1,30 @@
+ function changeImage() {
+
+        if (document.getElementById("battery").src == "http://www.userinterfaceicons.com/80x80/minimize.png") 
+        {
+            document.getElementById("battery").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+        }
+        else 
+        {
+            document.getElementById("battery").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+        }
+function changeImage1() {
+
+        if (document.getElementById("orange").src == "http://www.userinterfaceicons.com/80x80/minimize.png") 
+        {
+            document.getElementById("orange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+        }
+        else 
+        {
+            document.getElementById("orange").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+        }
+function changeImage2() {
+
+        if (document.getElementById("can").src == "http://www.userinterfaceicons.com/80x80/minimize.png") 
+        {
+            document.getElementById("can").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+        }
+        else 
+        {
+            document.getElementById("can").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+        }
